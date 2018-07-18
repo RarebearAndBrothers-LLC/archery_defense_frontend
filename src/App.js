b617b86fbc976e405bb7cd37a7d3f9b7a8bea6c3
Ryan Ghaida <ryaboug@gmail.com>
2018-07-18 11:43:56 -0400

    Added logic for arrow drawing

--- src/App.js
+++ src/App.js
@@ -10,31 +10,52 @@ class App extends Component {
     super();
 
     this.state = {
-      clicked: false 
+
+      arrowDrawn: false, 
+      arrowFired: false,
+
     }
 
   }
 
-  //Only begin drawback if onClick event happesn within a specified inner circle
-  //Otherwise, do not do anything. You dont need to amke a seperate component. 
-  handleDrawback = (event) => {
-    console.log(event.screenX, event.screenY)
-  }
 
   onMouseMove = (event) => {
     // Knows where mouse is on outer circle, this will be used
     // to render the movement of the arrow eventually. 
+    // console.log(event.screenX, event.screenY)
+  }
 
-    console.log(event.screenX, event.screenY)
+  // onMouseUp = (event) => {
+  //   console.log(event.screenX, event.screenY)
+  // }
+
+  onMouseDown = (event) => {
+    console.log("MouseDown", event.screenX, event.screenY)
     let x = event.screenX 
     let y = event.screenY
+
+    if(x >= -30 && x <= 0 && y >= -215 && y <= -185) {
+      //first click is within inner draw circle 
+      //User has now bgeun to draw arrow
+      
+      this.setState({arrowDrawn: true}, ()=> console.log(this.state))
+    } else if ( (x <= -30 || x >= 0) && (y <= -215 || y >= -185) && this.state.arrowDrawn === true) {
+      this.setState({arrowFired: true, arrowDrawn: false }, ()=> console.log("fired", this.state))
+    } else {
+      console.log("missed something")
+    }
+    console.log("done drawing")
+
+    
   }
+
+
 
 
   render() {
     return (
       <div className="App"  >
-        <DrawbackCircle  onMouseMove={this.onMouseMove}/>
+        <DrawbackCircle  onMouseUp={this.onMouseDown} onMouseDown={this.onMouseDown} onMouseMove={this.onMouseMove}/>
       </div>
 
     );
